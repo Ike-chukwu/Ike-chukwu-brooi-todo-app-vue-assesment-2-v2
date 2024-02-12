770494e7296fbf18f1fe758f75509479bdc103c1
Ike-chukwu <103510036+Ike-chukwu@users.noreply.github.com>
2024-02-12 23:12:06 +0100

    new remote

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,4 @@
 const { defineConfig } = require("@vue/cli-service");
-module.exports = defineConfig({
-  transpileDependencies: true,
+module.exports = {
   publicPath: "/brooi-todo-app-vue-assesment-2/",
-});
+};
